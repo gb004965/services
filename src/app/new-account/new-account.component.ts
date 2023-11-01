cd6636fbba420199b3dcf377cffd83d806069ebd
Rory O'Connor <gonzooo@gmail.com>
2023-11-01 22:48:35 +1000

    Modifications recorded during the Services Section

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,28 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component} from '@angular/core';
+import {LoggingService} from "../logging.service"; // note: still leave the LoggingService import
+import {AccountsService} from "../accounts.service";
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
-})
+  styleUrls: ['./new-account.component.css'],
+  providers: []  // note:  removed AccountsService to prevent Hierarchical Injection creating
+})                             // a new and different instance to the instance displayed in app-component.
+// note: Also removed LoggingService from providers[] as we have added it to app.module.ts
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService: LoggingService,  // note: still leave the LoggingService in the constructor
+              private accountsService: AccountsService) {
+
+      this.accountsService.statusUpdated.subscribe(
+        (status: string) => alert('New Status: ' + status)
+      );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountsService.addAccount(accountName, accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);    // removed now logged in from accountsService.
+    this.accountsService.statusUpdated.emit(accountStatus);
+
   }
 }
